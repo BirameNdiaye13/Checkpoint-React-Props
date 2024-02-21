2ab93226d01ffc04668e5f7e4fd2e70d02b4248e
MacBook Retina <macbookretina@192.168.1.5>
2024-02-21 23:54:20 +0000

    props

---
+++ src/players.js
@@ -0,0 +1,46 @@
+// src/players.js
+
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "FC Barcelone",
+      nationality: "Argentina",
+      jerseyNumber: 10,
+      age: 34,
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/250px-Lionel_Messi_WC2022.jpg",
+    },
+    // messi
+
+    {
+        name: "Cristiano Ronaldo",
+        team: "Real Madrid",
+        nationality: "Portugal",
+        jerseyNumber: 7,
+        age: 36,
+        image: "https://madeinfoot.ouest-france.fr/photos/liga/2018/zoom/cristiano-ronaldo-celebrates-after-scoring-during-the-liga-match-between-real-madrid-and-alaves-on-24th-february-2018-20180225131423-1819.jpg",
+      },
+      // ronaldo
+
+      {
+        name: "Neymar",
+        team: "Barcelone",
+        nationality: "Bresil",
+        jerseyNumber: 11,
+        age: 27,
+        image: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt5642fd24244c0b09/60decf07d731072d7c7765d8/3c9d9c9f2383594b627bdc7a74afbd8450dc372b.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+      },
+      // neymar
+
+      {
+        name: "Mbappe",
+        team: "Paris Saint-Germain",
+        nationality: "France",
+        jerseyNumber: 7,
+        age: 21,
+        image: "https://ds.static.rtbf.be/article/image/1920x1080/5/4/f/66705064b387572428517e38ae23e019-1708334443.jpg",
+      },
+      // neymar
+
+  ];
+  
+  export default players;
